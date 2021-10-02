b4141ad98874d82fe27377f364c8f3e7e4cfcd0d
Cabeleira <76449551+HubCabeleira@users.noreply.github.com>
2021-10-01 22:38:05 -0300

    Delete lib directory

--- lib/app/domain/interfaces/contactDao.dart
+++
@@ -1,9 +0,0 @@
-import 'package:agenda_crud/app/domain/entities/contact.dart';
-
-abstract class ContactDAO {
-  save(Contact contact);
-
-  remove(dynamic id);
-
-  Future<List<Contact>> find();
-}
--- lib/app/view/contact_form_back.g.dart
+++
@@ -1,9 +0,0 @@
-part of 'contact_form_back.dart';
-
-mixin _$ContactFormBack on _ContactFormBack, Store {
-  @override
-  String toString() {
-    return '''
-    ''';
-  }
-}
